a940ca3d48696ceb70b92e796207c3cdf2e5bcc5
Gabriel Staples <ercaguy@gmail.com>
2018-08-31 22:54:15 -0700

    Update "extract_odts.py" script to auto-scan for all .odt/.ods files

--- extracted_odts/extract_odts.py
+++ extracted_odts/extract_odts.py
@@ -12,16 +12,18 @@ So, this script will extract them to this folder so they are "git diffable". It 
 import zipfile
 import os # https://docs.python.org/dev/library/os.path.html#os.path.isdir
 import shutil # High-level file/folder manipulation - https://docs.python.org/3/library/shutil.html#shutil.rmtree
-import sys
+import sys # for getting the system argument vector
+import glob # For determining files in directories; see here: https://stackoverflow.com/a/3215392/4561887
 
 def extractOdts():
     print('Running Python script "{}".'.format(sys.argv[0]))
+    print('Scanning current directory for .odt and .ods files...')
 
-    # TODO: WRITE CODE TO HAVE IT SCAN THE DIRECTORY AND AUTOMATICALLY IDENTIFY ALL .ODT FILES
-    filepaths_src = [
-        './ward_bulletin_template.odt', 
-        './automated ward bulletin scripts - what to work on next - Gabriel.odt'
-    ]
+    # Get a list of all files in the current directory ending in .odt or .ods
+    filepaths_src = glob.glob("./*.odt")
+    filepaths_src += glob.glob("./*.ods")
+
+    print('{} files found.'.format(len(filepaths_src)))
 
     # print('Extracting these .odt files for easier version control:') 
     # for filepath in filepaths_src:
@@ -30,10 +32,12 @@ def extractOdts():
     paths_dest = []
     # print('Destination paths:')
     for path in filepaths_src:
-        # Get the destination path name, which is the last element after splitting by '/', minus
-        # the last 4 chars, which are the extension ('.odt')
-        path_dest = path.split('/')[-1][:-4]
-        # add './' to front of path
+        # Get the destination path name, which is the last element after splitting by '/', followed by replacing the 
+        # "." in the extension with "_". Ex: "myfile.odt" --> "myfile_odt"
+        path_dest = path.split('/')[-1]
+        path_dest = path_dest[:-4] + '_' + path_dest[-3:]
+
+        # add path prefix to front of path
         path_dest = './extracted_odts/' + path_dest
         paths_dest.append(path_dest)
         # print('  "{}"'.format(path_dest))
@@ -43,9 +47,10 @@ def extractOdts():
         source_file = filepaths_src[i]
         dest_dir = paths_dest[i]
 
-        print(('Extracting file "{}"\n' +
-               '  to dir "{}"').format(source_file, dest_dir))
+        print(('Extracting file {}: "{}"\n' +
+               '  to dir "{}"').format(i+1, source_file, dest_dir))
 
+        # Do the zip file stuff!
         zip_ref = zipfile.ZipFile(source_file, 'r')
         # Delete the destination dir if it already exists
         if (os.path.isdir(dest_dir)):
