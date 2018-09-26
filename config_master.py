da68bef1d97dbd5156baedfeb18fadd9d0304a03
Gabriel Staples <ercaguy@gmail.com>
2018-09-25 19:11:58 -0700

    Update config_master.py with details about how I'll implement the "auto" feature

--- config_master.py
+++ config_master.py
@@ -1,52 +1,44 @@
 """
-# config_selector.py
-- Set which config.py file to import: the demonstration one, or the one for your particular ward.
+# config_master.py
+- the master configuration file
 
 LibreBulletin automated bulletin generation
 By Gabriel Staples
 https://github.com/ElectricRCAircraftGuy/LibreBulletin
 """
 
-# # Set "configfile" to "default" to import "config.py"demonstrate this library with example input files which I provide, or set to False to use your
-# # real input files. My input files for generating the real bulletin for my ward are stored in a subfolder here called
-# # "PERSONAL_INFO_NOT_FOR_REPO". Since this subfolder also has an empty file in it called "__init__.py", it is also
-# # considerd a Python "Package", and hence "PERSONAL_INFO_NOT_FOR_REPO/filename.py" type Python "modules" can be imported
-# # using import statements as shown below.
-# # For more on Python import statements and packages, see:
-# # - *****+https://docs.python.org/3/tutorial/modules.html#packages
-# # - *****+https://stackoverflow.com/questions/1260792/import-a-file-from-a-subdirectory/1260846#1260846
-# # - *****https://stackoverflow.com/questions/8953844/import-module-from-subfolder/8954533
-
-# configfile = "default"
-# configfile = "custom"
-
-# if (demo == True):
-#     # Import the demonstration config.py file
-#     import config
-# else:
-#     # Import my personal (not shared) config.py file for my ward specifically
-#     from PERSONAL_INFO_NOT_FOR_REPO import config
-
-# import bulletin_find_and_replace
-
-# demo = True
-
-# if (demo == True):
-#     
-
 # ----------------------------------------------------------------------------------------------------------------------
 # USER PARAMETERS
 # Update these parameters to customize your bulletin!
 # ----------------------------------------------------------------------------------------------------------------------
 
-# Set to True to demonstrate this library with example input files, or set to False to use your real input files
-# (which are, in my case for generating the real bulletin for my ward, written below)
+# Set to True to demonstrate this library with example input files I have already provided, or set to False to use your
+# real input files (which are, in my case for generating the real bulletin for my ward, written below)
 demo = True
 # demo = False
 
-# In case you use `demo = False` above, pull the bulletin inputs from this file:
-# bulletin_INPUTS = "auto" 
-bulletin_INPUTS = "./MY_PERSONAL_WARD_INFO/20180930#1--2bulletin_INPUTS.txt"
+# In case you use `demo = False` above, pull the bulletin inputs from this bulletin_INPUTS file:
+# - Set it to "auto" to force the program to automatically choose the bulletin_INPUTS file for you!
+# - It will do this by scanning for all relevant files in the "auto_search_folder", set below, ensuring their names
+# match the "auto_search_pattern" regular expression set below, alphabetizing them, then choosing the *last* one in the
+# list! This way you can name them chronologically, for instance, as "YYYYMMDDn--filename.txt"  (Ex: "20180930#1--
+# myWardBulletinInputs.txt", for your bulletin inputs for 30 Sept. 2018). Now, by simply using the "auto" setting below,
+# with the correct "auto_search_pattern", you can manually copy and paste your .txt file each new week, then increment
+# the date at the front of its name, and the script will automatically pull this, the latest file when  auto-building
+# the bulletin!
+# bulletin_INPUTS = "./MY_PERSONAL_WARD_INFO/20180930#1--2bulletin_INPUTS.txt"
+bulletin_INPUTS = "auto" # Uncomment to automatically use your latest bulletin_INPUTS file instead, as described above.
+
+# Only used if `bulletin_INPUTS = "auto"`:
+# Automatically search for the last-alphabetically-listed *.txt file in this folder, to be used as your bulletin inputs
+# file:
+auto_search_folder = "./MY_PERSONAL_WARD_INFO"
+# Only search for files whose names match this *regular expression* search pattern.
+# See: 1) https://en.wikipedia.org/wiki/Regular_expression.
+#      2) Regex tester: https://regex101.com/
+# Ex: "[0-9]{8}.?.?--.*\.txt" will match any file names that look like "20180930#1--myWardBulletinInputs.txt", or 
+# "20180930--myWardBulletinInputs.txt", etc.
+auto_search_pattern = "[0-9]{8}.?.?--.*\.txt"
 
 # ----------------------------------------------------------------------------------------------------------------------
 # PROGRAMMER PARAMETERS (not intended to be changed by user)
